1ff278c991279c40a5a4baae2dc3afebaf1bd75a
Leonardo Ross Torquato Lima <leonardortlima@gmail.com>
2014-02-28 11:34:44 -0300

    atualizando

--- src/main/js/Config.persys.js
+++ src/main/js/Config.persys.js
@@ -162,11 +162,16 @@ define(function(){
 						]
 					},
 					{
-						label: "Pesquisa por ID",
+						label: "Pesquisar por ID",
 						url: "/produto/{id}",
 						method: "GET",
+<<<<<<< Upstream, based on origin/interface
 						description: "Pesquisar uma base de conhecimento a partir do seu ID.",
 						returns: "{\"id\":\"21\",\"codigo\":\"PBSC20\",\"dataCriacao\":\"1393596044194\",\"dataAlteracao\":\"1393596044194\",\"statusModel\":\"1\",\"permitidoExcluir\":\"true\",\"permitidoAlterar\":\"true\",\"titulo\":\"TITULO DA BASE DE CONHECIMENTO\",\"tipo\":\"Interno\",\"mensagem\":\"BASE DE CONHECIMENTO NOVA\"}",
+=======
+						result: "{\"id\":\"1\",\"codigo\":\"PBSC0\",\"dataCriacao\":\"1391457415467\",\"dataAlteracao\":\"1391605534367\",\"statusModel\":\"1\",\"permitidoExcluir\":\"true\",\"permitidoAlterar\":\"true\",\"titulo\":\"Cabo de Rede Externa Rompido Externo\",\"tipo\":\"Interno\",\"mensagem\":\"Cabo de rede ou óptico rompido do Poste de entrada, para fora.\n\"}",
+						description: "Pesquisar uma base de conhecimento a partir do seu ID",
+>>>>>>> 50512c6 Atualizando base de conhecimento
 						params: [
 							{
 								name: "id",
@@ -179,13 +184,21 @@ define(function(){
 						label: "Alterar por ID",
 						url: "/produto/{id}",
 						method: "PUT",
+<<<<<<< Upstream, based on origin/interface
 						description: "Alterar uma base de conhecimento a partir do seu ID.",
 						result: "{\"statusModel\": \"1\",\"permitidoExcluir\": \"true\",\"permitidoAlterar\": \"true\",\"titulo\": \"TITULO DA BASE DE CONHECIMENTO MODIFICADO\",\"tipo\": \"Interno\",\"mensagem\": \"BASE DE CONHECIMENTO MODIFICADA\"}",
 						returns: "{\"id\":\"19\",\"codigo\":\"\",\"dataCriacao\":\"1393422672704\",\"dataAlteracao\":\"1393597662334\",\"statusModel\":\"1\",\"permitidoExcluir\":\"true\",\"permitidoAlterar\":\"true\",\"titulo\":\"TITULO DA BASE DE CONHECIMENTO MODIFICADO\",\"tipo\":\"Interno\",\"mensagem\":\"BASE DE CONHECIMENTO MODIFICADA\"}",
+=======
+						description: "Alterar uma base de conhecimento a partir do seu ID",
+>>>>>>> 50512c6 Atualizando base de conhecimento
 						params: [
 							{
 								name: "id",
+<<<<<<< Upstream, based on origin/interface
 								description: "Identificador da Base de Conhecimento.",
+=======
+								description: "Identificador do produto",
+>>>>>>> 50512c6 Atualizando base de conhecimento
 								type: "long",
 							}
 						]
