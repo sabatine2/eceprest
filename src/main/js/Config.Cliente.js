f145b077c1e906c6d7d3e9c4e32164b23ddac977
Leonardo Ross Torquato Lima <leonardortlima@gmail.com>
2014-03-06 12:28:05 -0300

    Atualizando Cliente

--- src/main/js/Config.Cliente.js
+++ src/main/js/Config.Cliente.js
@@ -78,30 +78,28 @@ define(function(){
 							{
 								name: "nomeUsuario",
 								description: "Nome de usuário do cliente.",
-								type: "string [100]",
+								type: "",
 								insert: true,
 								required: true,
-								modifiable: true,
 							},
 							{
 								name: "razaoNome",
 								description: "Nome ou Razão Social do cliente.",
-								type: "string [20]",
+								type: "",
 								insert: true,
 								modifiable: true,
 							},
 							{
 								name: "fantasiaSobrenome",
 								description: "Sobrenome ou Nome Fantasia do cliente.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
-								modifiable: true,
 							},
 							{
 								name: "cnpjCpf",
 								description: "CNPJ ou CPF do cliente.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
 								modifiable: true,
@@ -109,7 +107,15 @@ define(function(){
 							{
 								name: "ieRg",
 								description: "Inscrição estadual ou RG do cliente.",
-								type: "string [1000]",
+								type: "",
+								insert: true,
+								required: true,
+								modifiable: true,
+							},
+							{
+								name: "dataNascimento",
+								description: "Data de Nascimento do cliente.",
+								type: "",
 								insert: true,
 								required: true,
 								modifiable: true,
@@ -117,7 +123,7 @@ define(function(){
 							{
 								name: "emailPrincipal",
 								description: "E-mail do cliente.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
 								modifiable: true,
@@ -125,7 +131,7 @@ define(function(){
 							{
 								name: "nota",
 								description: "Anotação sobre o cliente.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
 								modifiable: true,
@@ -133,47 +139,42 @@ define(function(){
 							{
 								name: "dashboardNome",
 								description: "Nome do cliente no Dashboard.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
-								modifiable: true,
 							},
 							{
 								name: "timezone",
 								description: "Fuso Horário do cliente.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
-								modifiable: true,
 							},
 							{
 								name: "locale",
 								description: "Localização do cliente.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
-								modifiable: true,
 							},
 							{
 								name: "contato",
 								description: "Informações de contato do cliente.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
-								modifiable: true,
 							},
 							{
 								name: "situacao",
 								description: "Situação do cliente.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
-								modifiable: true,
 							},
 							{
 								name: "tipoCliente",
 								description: "Tipo de cliente.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
 								modifiable: true,
@@ -181,18 +182,16 @@ define(function(){
 							{
 								name: "site",
 								description: "Site do cliente.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
-								modifiable: true,
 							},
 							{
 								name: "imagem",
 								description: "Imagem do cliente.",
-								type: "string [1000]",
+								type: "",
 								insert: true,
 								required: true,
-								modifiable: true,
 							}
 						]
 					},
@@ -212,36 +211,31 @@ define(function(){
 						url: "/cliente",
 						method: "GET",
 						returns: "{\"data\":[{\"id\":\"1\",\"codigo\":\"PBSC0\",\"statusModel\":\"1\",\"permitidoExcluir\":\"true\",\"permitidoAlterar\":\"true\",\"titulo\":\"Cabo de Rede Externa Rompido Externo\",\"tipo\":\"Interno\"},{\"id\":\"2\",\"codigo\":\"PBSC1\",\"statusModel\":\"1\",\"permitidoExcluir\":\"true\",\"permitidoAlterar\":\"true\",\"titulo\":\"Cabo de Rede Interna Rompido \",\"tipo\":\"Externo\"}],\"paging\":{\"@numberPages\":\"1\",\"@number\":\"15\",\"@page\":\"1\",\"@type\":\"application/json\"}}",
-						description: "Listar todas as bases de conhecimento cadastradas. Podem ser utilizados os seguintes filtros:",
+						description: "Listar todas clientes. Podem ser utilizados os seguintes filtros:",
 						params: [
 							{
-								
 								name: "since",
-								description: "Filtrar pela data da última alteracao da Base de Conhecimento. Formato dd/mm/yyy.",
-								type: "timestamp",
+								description: "Filtrar pela data da última alteracao do cliente. Formato dd/mm/yyy. Valor padrão: \"01/01/1900\"."
 							},
 							{
 								name: "offset",
-								description: "Filtrar a partir do número de um resultado específico.",
-								type: "int",
+								description: "Filtrar a partir do número de um resultado específico. Valor padrão: \"0\"."
 							},
 							{
 								name: "limit",
-								description: "Limitar o número de resultados buscados.",
-								type: "int",
+								description: "Limitar o número de resultados buscados. Valor padrão: \"10\"."
 							},
 							{
 								name: "status",
-								description: "Status: 0 Inativo, 1 Ativo, 2 Removido.",
-								type: "int",
+								description: "Status 0 Inativo, 1 Ativo, 2 Removido. Valor padrão: \"2\"."
 							}
 						]
 					},
 					{
 						label: "Pesquisar por ID",
-						url: /cliente/{id}",
+						url: "/cliente/{id}",
 						method: "GET",
-						description: "Pesquisar uma base de conhecimento a partir do seu ID.",
+						description: "Pesquisar um clienteo a partir do seu ID.",
 						returns: "{\"id\":\"1\",\"codigo\":\"PBSC0\",\"dataCriacao\":\"1391457415467\",\"dataAlteracao\":\"1391605534367\",\"statusModel\":\"1\",\"permitidoExcluir\":\"true\",\"permitidoAlterar\":\"true\",\"titulo\":\"Cabo de Rede Externa Rompido Externo\",\"tipo\":\"Interno\",\"mensagem\":\"Cabo de rede ou �ptico rompido do Poste de entrada, para fora.\n\"}",
 						description: "Pesquisar uma base de conhecimento a partir do seu ID",
 						params: [
@@ -252,7 +246,7 @@ define(function(){
 						label: "Alterar por ID",
 						url: "/cliente/{id}",
 						method: "PUT",
-						description: "Alterar uma base de conhecimento a partir do seu ID.",
+						description: "Alterar um cliente a partir do seu ID.",
 						result: "{\"titulo\": \"TITULO DA BASE DE CONHECIMENTO MODIFICADO\",\"tipo\": \"Interno\",\"mensagem\": \"BASE DE CONHECIMENTO MODIFICADA\"}",
 						returns: "{\"id\":\"19\",\"codigo\":\"\",\"dataCriacao\":\"1393422672704\",\"dataAlteracao\":\"1393597662334\",\"statusModel\":\"1\",\"permitidoExcluir\":\"true\",\"permitidoAlterar\":\"true\",\"titulo\":\"TITULO DA BASE DE CONHECIMENTO MODIFICADO\",\"tipo\":\"Interno\",\"mensagem\":\"BASE DE CONHECIMENTO MODIFICADA\"}",
 						params: [
@@ -263,7 +257,7 @@ define(function(){
 						label: "Remover por ID",
 						url: "/cliente/{id}",
 						method: "DELETE",
-						description: "Remover uma Base de Conhecimento a partir do seu ID.",
+						description: "Remover um cliente a partir do seu ID.",
 						params: [
 
 						]
